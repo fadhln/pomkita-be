5671439af90b1a571c676c65673283ee7600782c
fadhln <fadhlan1337@gmail.com>
2026-09-12 02:22:36 +0000

    B1: add draft lease fencing and read procedure

--- test/b0_helpers_test.go
+++ test/b0_helpers_test.go
@@ -12,6 +12,9 @@ func resetB0Foundation(t *testing.T, conn *pgx.Conn, withContextFunction bool) {
 	t.Helper()
 	ctx := context.Background()
 	root := repositoryRoot(t)
+	if _, err := conn.Exec(ctx, readMigration(t, root, "000006_b1_drafts.down.sql")); err != nil {
+		t.Fatalf("reset B1 drafts: %v", err)
+	}
 	if _, err := conn.Exec(ctx, readMigration(t, root, "000005_b1_shifts.down.sql")); err != nil {
 		t.Fatalf("reset B1 shifts: %v", err)
 	}
