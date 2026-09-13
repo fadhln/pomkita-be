231915d1597ec4e787133d25c955372b3c6c8d9f
fadhln <fadhlan1337@gmail.com>
2026-09-13 11:09:45 +0000

    B2: add amendment HTTP endpoints

--- internal/httpapi/router.go
+++ internal/httpapi/router.go
@@ -103,6 +103,9 @@ func registerGovernanceRoutes(router *gin.Engine, verifier TokenVerifier, servic
 	protectedRead := []gin.HandlerFunc{AuthMiddleware(verifier)}
 	router.POST("/shift/ack", append(protectedWrite, ackShiftHandler(service))...)
 	router.POST("/amendment/approve", append(protectedWrite, approveAmendmentHandler(service))...)
+	router.POST("/amendment/request", append(protectedWrite, requestAmendmentHandler(service))...)
+	router.POST("/amendment/reject", append(protectedWrite, rejectAmendmentHandler(service))...)
+	router.GET("/amendments", append(protectedRead, readAmendmentQueueHandler(service))...)
 	router.GET("/anomalies", append(protectedRead, readGovernanceAnomaliesHandler(service))...)
 }
 
