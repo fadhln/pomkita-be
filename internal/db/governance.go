43febdd9f2a90a3f9c7c811d8cbc43dd993d9f67
fadhln <fadhlan1337@gmail.com>
2026-09-15 00:41:58 +0700

    B7: remove compatibility relay package

--- internal/db/governance.go
+++ internal/db/governance.go
@@ -10,7 +10,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
-	"github.com/pomkita/pomkita-be/internal/relay"
+	"github.com/pomkita/pomkita-be/internal/service/relay"
 )
 
 // Claim implements the relay.Store claim operation.
